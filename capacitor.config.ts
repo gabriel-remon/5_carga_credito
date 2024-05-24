5b43c2f1b9916fc440652a209f693387b1ed3e86
gabriel-remon <remongabi16@gmail.com>
2024-05-23 23:32:39 -0300

    base creada

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'appTest',
+  appId: 'io.ionic.cargaapp',
+  appName: 'cargaApp',
   webDir: 'www'
 };
 
